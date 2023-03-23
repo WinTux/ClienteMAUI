bebe7ff1c9f86637a0dcc6ebd59ec12fc5eff730
Rubén Gonzalo Soria Soria <rusok.trabajo@hotmail.com>
2023-03-22 21:24:43 -0400

    Eventos en GestionPlatosPage

--- ClienteMAUI/Pages/GestionPlatosPage.xaml.cs
+++ ClienteMAUI/Pages/GestionPlatosPage.xaml.cs
@@ -1,8 +1,10 @@
+//using Android.OS;
 using ClienteMAUI.ConexionDatos;
 using ClienteMAUI.Models;
+using System.Diagnostics;
 
 namespace ClienteMAUI.Pages;
-
+[QueryProperty(nameof(Plato), "Plato")]
 public partial class GestionPlatosPage : ContentPage
 {
     private readonly IRestConexionDatos conexionDatos;
@@ -14,7 +16,6 @@ public partial class GestionPlatosPage : ContentPage
 			_esNuevo = esNuevo(value);
 			_plato = value;
 			OnPropertyChanged();//Obligatorio al actualizar un plato existente
-			//Nos toca: Manejo de los parámetros que se envían desde MainPage
 		}
 	}
     public GestionPlatosPage(IRestConexionDatos conexionDatos)
@@ -26,4 +27,24 @@ public partial class GestionPlatosPage : ContentPage
 	bool esNuevo(Plato plato) {
 		return plato.Id == 0 ? true : false;
 	}
+	async void OnCancelarClic(object sender, EventArgs e) {
+		await Shell.Current.GoToAsync("..");
+	}
+	async void OnGuardarClic(object sender, EventArgs e)
+	{
+		if (_esNuevo)
+		{
+			Debug.WriteLine("[REGISTRO] Agregando un nuevo plato");
+			await conexionDatos.AddPlatoAsync(_plato);
+		}
+		else {
+            Debug.WriteLine("[REGISTRO] Modificando un plato");
+            await conexionDatos.UpdatePlatoAsync(_plato);
+        }
+        await Shell.Current.GoToAsync("..");
+    }
+	async void OnEliminarClic(object sender, EventArgs e) {
+		await conexionDatos.DeletePlatoAsync(_plato.Id);
+        await Shell.Current.GoToAsync("..");
+    }
 }
